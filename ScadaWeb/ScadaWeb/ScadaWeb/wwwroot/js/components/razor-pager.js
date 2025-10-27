84d50a4bd37d8bc9a60d8362eb015b8ff62efe40
2mik <m_shir@mail.ru>
2025-10-27 18:01:42 +0300

    Update razor-pager.js

--- ScadaWeb/ScadaWeb/ScadaWeb/wwwroot/js/components/razor-pager.js
+++ ScadaWeb/ScadaWeb/ScadaWeb/wwwroot/js/components/razor-pager.js
@@ -3,43 +3,56 @@
 class RazorPager {
     // Notifies that a page item has been clicked.
     static PAGE_CLICK_EVENT = "pageclick.rs.pager";
+    // The CSS class of the pager.
+    static PAGER_CLASS = ".rs-pager";
 
     // The default pager options.
     static defaultOptions = {
         submitOnClick: false // submits the form when a page is clicked
     };
 
-    pagerElem; // jQuery object that represents the pager
-    options;   // pager options
+    pagerElemID; // ID of the HTML element representing the pager
+    options;     // pager options
 
     constructor(pagerElemID, opt_options) {
-        this.pagerElem = $("#" + pagerElemID);
+        this.pagerElemID = pagerElemID;
         this.options = Object.assign({}, RazorPager.defaultOptions, opt_options);
-
-        this.pagerElem.find("a.page-link").on("click", (event) => {
-            this._handlePageClick($(event.target));
-            event.preventDefault();
-        });
+        this.bindEvents();
     }
 
     // Handles a user click on a page item.
     _handlePageClick(linkElem) {
         let pageIndex = linkElem.data("page");
-        this.pagerElem.closest(".rs-pager").find("input:hidden:first").val(pageIndex);
+        let pagerElem = linkElem.closest(".rs-pager");
+        pagerElem.find("input:hidden:first").val(pageIndex);
 
         if (this.options.submitOnClick) {
-            this.pagerElem.closest("form").trigger("submit");
+            pagerElem.closest("form").trigger("submit");
         } else {
-            this.pagerElem.trigger(RazorPager.PAGE_CLICK_EVENT, pageIndex);
+            pagerElem.trigger(RazorPager.PAGE_CLICK_EVENT, pageIndex);
         }
+    }
+
+    // Returns a jQuery object representing the pager.
+    _getPagerElem() {
+        return $("#" + this.pagerElemID);
+    }
+
+    // Binds events to the DOM elements.
+    bindEvents() {
+        this._getPagerElem().on("click", "a.page-link", (event) => {
+            this._handlePageClick($(event.target));
+            event.preventDefault();
+        });
     }
 
     // Sets the current page index to zero.
     reset(opt_submit) {
-        this.pagerElem.find("input:hidden:first").val(0);
+        let pagerElem = this._getPagerElem();
+        pagerElem.find("input:hidden:first").val(0);
 
         if (opt_submit) {
-            this.pagerElem.closest("form").trigger("submit");
+            pagerElem.closest("form").trigger("submit");
         }
     }
 }
